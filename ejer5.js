e909980ef8d1650f895c35fff52437d399d7d521
Evelin <eveliz2001@gmail.com>
2024-08-12 16:04:47 -0400

    ajustes

--- ejer5.js
+++ ejer5.js
@@ -1,30 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const text1 = document.getElementById('text1');
-    const text2 = document.getElementById('text2');
-    const caja2 = document.getElementById('caja2');
-    const img = document.getElementById('img');
-    const caja3 = document.getElementById('caja3');
-
+$(document).ready(function() {
     // Mostrar y ocultar el segundo <div> al pasar el mouse por el primer <div>
-    text1.addEventListener('mouseover', () => {
-        text2.classList.remove('hidden');
-    });
+    $('#text1').hover(
+        function() {
+            $('#text2').removeClass('hidden');
+        },
+        function() {
+            $('#text2').addClass('hidden');
+        }
+    );
 
-    text1.addEventListener('mouseout', () => {
-        text2.classList.add('hidden');
-    });
-
-    // Agrandar la imagen al hacer clic en el <div> id "caja2" y volver a su tamaño inicial al salir
-    caja2.addEventListener('click', () => {
-        img.classList.add('enlarged');
-    });
-
-    caja2.addEventListener('mouseleave', () => {
-        img.classList.remove('enlarged');
+    // Alternar el tamaño de la imagen al hacer clic en el <div> id "caja2"
+    $('#caja2').click(function() {
+        $('#img').toggleClass('enlarged');
     });
 
     // Agrandar el texto al hacer doble clic en el <div> id "caja3"
-    caja3.addEventListener('dblclick', () => {
-        caja3.classList.toggle('large-text');
+    $('#caja3').dblclick(function() {
+        $(this).toggleClass('large-text');
     });
 });
+
+
